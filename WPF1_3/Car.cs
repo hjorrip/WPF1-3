2ceeb4c0d02526a10044582fbd653fbe641db26d
Hjörtur Pálmi Pálsson <hjorturpp@ntv.is>
2019-05-15 23:49:01 +0000

    first commit

---
+++ WPF1_3/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1_3
+{
+
+    public class CarInfo
+    {
+        public Car[] results { get; set; }
+    }
+
+    public class Car
+    {
+        public string type { get; set; }
+        public string subType { get; set; }
+        public string color { get; set; }
+        public string registryNumber { get; set; }
+        public string number { get; set; }
+        public string factoryNumber { get; set; }
+        public string registeredAt { get; set; }
+        public string pollution { get; set; }
+        public string weight { get; set; }
+        public string status { get; set; }
+        public string nextCheck { get; set; }
+    }
+
+}
---
+++ WPF1_3/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                WebClient web = new WebClient();
+                web.Encoding = Encoding.UTF8;
+                string json = web.DownloadString(@"https://apis.is/car?number=" + txtCarnumber.Text);
+                CarInfo carinfo = JsonConvert.DeserializeObject<CarInfo>(json);
+                Car c = carinfo.results[0];
+
+                txtType.Text = c.type;
+                txtColor.Text = c.color;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bíll með númerið " + txtCarnumber.Text + " fannst ekki");
+            }
+ 
+
+        }
+    }
+}
